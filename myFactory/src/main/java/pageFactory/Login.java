bfe309082c863853862c7b666774b196ef3a5b11
chendany <63691805+chendany@users.noreply.github.com>
2020-04-24 22:35:12 +0800

    Update Login.java

--- myFactory/src/main/java/pageFactory/Login.java
+++ myFactory/src/main/java/pageFactory/Login.java
@@ -7,50 +7,41 @@ import org.openqa.selenium.support.PageFactory;
 
 public class Login {
     WebDriver driver;
-
+   //用户名框
     @FindBy(name="uid")
     WebElement userName;
-
+    //密码框
     @FindBy(name="passw")
     WebElement password;
-
+    //网站名
     @FindBy(xpath="/html/head/title")
     WebElement titleText;
-
+    //登录按钮
     @FindBy(name="btnSubmit")
     WebElement login;
-
+    
     public Login(WebDriver driver){
         this.driver = driver;
-        //This initElements method will create  all WebElements
         PageFactory.initElements(driver, this);
     }
-    //Set user name in textbox
+    //输入用户名
     public void setUserName(String strUserName){
         userName.sendKeys(strUserName);
 
     }
-
-    //Set password in password textbox
+    //输入密码 
     public void setPassword(String strPassword){
         password.sendKeys(strPassword);
     }
-
-    //Click on login button
+    //点击登录
     public void clickLogin(){
         login.click();
     }
-
-    //Get the title of Login Page
+    //获取网站名
     public String getLoginTitle(){
         return	titleText.getText();
     }
-    /**
-     * This POM method will be exposed in test case to login in the application
-     * @param strUserName
-     * @param strPasword
-     * @return
-     */
+    //登录
     public void loginTo(String strUserName,String strPasword){
         //Fill user name
         this.setUserName(strUserName);
